dd17f0d3415f1c771abaf4134f499baa0337bd85
appletreeman <chris.hodgkinson@gmail.com>
2018-01-06 17:50:29 +0000

    add support files

---
+++ config.lua
@@ -0,0 +1,11 @@
+-- config.lua
+
+application =
+{
+	content =
+	{
+	    width = 640,
+		height = 960,
+		scale = "letterbox"
+	},
+}
---
+++ main.lua
@@ -0,0 +1,18 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+display.setStatusBar( display.HiddenStatusBar )
+
+
+
+
+local composer = require "composer"
+display.setDefault("background", .9)
+
+
+-- load menu screen
+composer.gotoScene( "menu" )
+
